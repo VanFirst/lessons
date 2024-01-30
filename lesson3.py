0af9cf15f077dcd169b8b493e756834d2cb66341
Van <Boxinet@mail.ru>
2024-01-30 13:37:26 +0300

    initial commit

--- lesson3.py
+++ lesson3.py
@@ -1,10 +1,39 @@
 #x = 4
 #print(id(x))
+import math
 
 #print('Hello ' + 'World')
 #name = input('Enter your name: ')
 #print('Your name is ' + name)
 
-x = int(input('введите число: '))
-y = 5
-print(x+y)
+#x = int(input('введите число: '))
+#y = 5
+#print(x+y)
+
+#import math
+#seats = False
+#money = True
+#if seats and money:
+#        print('Идём в кино')
+#else:
+#       print('Не идём')
+#x = math.sqrt(6)
+#print(x)
+
+#line = 'Hello\n world\'\\'
+#print(line)
+
+#text = 'Hello'
+#substring = text[1:-1]
+#print(substring)
+
+x = 5
+y = 'Hello'
+print(f'{x} + {y}')
+
+x = 5
+print(x)
+x = 5+5
+print(x)
+x += 5
+print(x)
