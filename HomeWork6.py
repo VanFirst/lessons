84ef06809172f54357e01ea561c7ec003cd2f36e
Van <Boxinet@mail.ru>
2024-02-12 22:31:23 +0300

    initial commit

--- HomeWork6.py
+++ HomeWork6.py
@@ -1,15 +1,15 @@
 
-def x(a, b):
-   w = int(a + b)
-   return w
-
-sum = 0
-a = 1
-while a <= 20:
-    sum = x(a, sum)
-    a += 1
-
-print(sum)
+# def x(a, b):
+#    w = int(a + b)
+#    return w
+#
+# sum = 0
+# a = 1
+# while a <= 20:
+#     sum = x(a, sum)
+#     a += 1
+#
+# print(sum)
 
 
 
@@ -52,19 +52,52 @@ print(sum)
 
 
 
-def year(a):
-    if a == 12 or a == 1 or a == 2:
-        return 'winter'
-    elif a == 3 or a == 4 or a == 5:
-        return 'spring'
-    elif a == 6 or a == 7 or a == 8:
-        return 'summer'
-    elif a == 9 or a == 10 or a == 11:
-        return 'autumn'
-    else:
-        return 'Error month'
+# def year(a):
+#     if a == 12 or a == 1 or a == 2:
+#         return 'winter'
+#     elif a == 3 or a == 4 or a == 5:
+#         return 'spring'
+#     elif a == 6 or a == 7 or a == 8:
+#         return 'summer'
+#     elif a == 9 or a == 10 or a == 11:
+#         return 'autumn'
+#     else:
+#         return 'Error month'
+#
+# #print(year(int(input('Введите номер месяца от 1 до 12: '))))
+#
+# a = input('Введите номер месяца от 1 до 12: ')
+# print(year(int(a)))
 
-#print(year(int(input('Введите номер месяца от 1 до 12: '))))
 
-a = input('Введите номер месяца от 1 до 12: ')
-print(year(int(a)))
+# a = [4, 7, 3, 6, 12, 55, 2]
+# def low(a):
+#     a.sort()
+#     return a[1]
+# print(low(a))
+
+
+
+
+
+# r = float(input('Введите радиус: '))
+# p = 3.14
+# def circle(r, p):
+#     s = p * r * r
+#     return s
+#
+# print(circle(r, p))
+
+
+
+
+# import random
+# def rand():
+#     list = ['Да', 'Нет', 'Не могу сейчас сказать']
+#     w = random.randint(0, 2)
+#     return list[w]
+# input(f'Привет бро! \nЗадавай волнующий тебя вопрос, на который можно ответить да или нет: ')
+# print(rand())
+
+
+
